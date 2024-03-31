058a8d3e79704daa5f0fb4fa2c2ccde81a165cb8
Leticia0110 <163892605+Leticia0110@users.noreply.github.com>
2024-03-31 19:51:10 -0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,13 @@
+namespace AMS24_MyClass.Models
+/// <summary>description</summary>
+/// <author>Leticia</author>
+{
+    public class Client
+    {
+        public int Year { get; set;}
+        public int Telephone { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Weigth { get; set; }
+    }
+}
